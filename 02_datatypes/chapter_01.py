2d1cbca2c3a84e8aa60b3dd68cb0bfbf0e163591
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-07 22:56:51 +0530

    Learn: about objects -  mutable & immutable.

---
+++ 02_datatypes/chapter_01.py
@@ -0,0 +1,8 @@
+sugar_amount = 2
+print(f"Initial sugar: {sugar_amount}")
+
+sugar_amount = 21
+print(f"Second Initial sugar: {sugar_amount}")
+
+print(f"ID of 2: {id(2)}")
+print(f"ID of 21: {id(21)}")
---
+++ 02_datatypes/chapter_02.py
@@ -0,0 +1,8 @@
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+spice_mix.add("Ginger")
+spice_mix.add("Cadamom")
+spice_mix.add("Lemon")
+print(f"Initial spice mix id: {spice_mix}")
+print(f"Initial spice mix id: {id(spice_mix)}")

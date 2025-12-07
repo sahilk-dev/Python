4db6782462e5d2a054a72bc344aafe29ab46e00e
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-07 22:10:56 +0530

    initial commit

---
+++ 01_Basics/hello.py
@@ -0,0 +1,13 @@
+# print("hello world!", end=" ")
+# print("dsfksnc sdfjsdc")
+
+# print(3)
+# print(568)
+
+# print(3 + 3)
+# print(3 * 3)
+# print(3 - 3)
+# print(3 % 3)
+# print(3 / 3)
+
+print("Hello, My name is Sahil Kamila", "and my age is:", 22)

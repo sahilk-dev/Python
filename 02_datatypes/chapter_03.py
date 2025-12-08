85ccaeb4181c6cb645fbd4cf2860ba8957f1c2ae
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-08 22:57:15 +0530

    Learn: Numbers, Booleans and operators.

---
+++ 02_datatypes/chapter_03.py
@@ -0,0 +1,34 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 2
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {remaining_tea}")
+
+milk_liters = 7
+servings = 4
+milk_per_serving = milk_liters / servings
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bags = 7
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"whole tea bags per pot: {bags_per_pot}")
+
+total_cadamom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cadamom_pods % pods_per_cup
+print(f"Leftover C pods {leftover_pods}")
+
+base_flavour_strength = 2
+scale_factor = 3
+powerful_flovour = base_flavour_strength ** scale_factor
+print(f"Scaled flavour strength {powerful_flovour}")
+# 2 * 2 * 2
+
+total_tea_leaves_harvested = 1_000_000_000
+print(f"tea leaves: {total_tea_leaves_harvested}")

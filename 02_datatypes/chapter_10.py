fb88659457a5ad4400c4552eece86c5d7d0ec419
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-14 11:37:10 +0530

    Learn: Dictionary with methods.

---
+++ 02_datatypes/chapter_10.py
@@ -0,0 +1,33 @@
+# Dictionary
+
+chai_order = dict(type="Masala Chai", size= "Large", sugar=2)
+# print(f"Chai order: {chai_order}")
+
+chai_recipe = {}
+chai_recipe["base"] = "black tea"
+chai_recipe["liquid"] = "milk"
+
+# print(f"Recipe base: {chai_recipe["base"]}")
+# print(f"Recipe base: {chai_recipe}")
+del chai_recipe["liquid"] # For Deleting
+# print(f"Recipe: {chai_recipe}")
+
+# print(f"Is sugar in the order? {'sugar' in chai_order}") # Membership test
+
+chai_order = {"type": "Ginger Chai", "size": "Medium", "sugar": 1}
+
+# print(f"Order details (keys): {chai_order.keys()}")
+# print(f"Order details (values): {chai_order.values()}")
+# print(f"Order details (items): {chai_order.items()}")
+
+# Removes the items
+last_item = chai_order.popitem()
+print(f"Removed last item: {last_item}")
+
+extra_spices = {"cardamom": "crushed", "ginger": "sliced"}
+chai_recipe.update(extra_spices)
+
+print(f"Updated chai recipe: {chai_recipe}")
+
+Customer_note = chai_order.get("size", "No Note")
+print(f"Customer_note is: {Customer_note}")

019038ae3d7925f9502f58fbd5d17db5cbe1ec02
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-18 11:11:54 +0530

    Learn: For loop with some real world examples.

---
+++ 04_loops/02_batch_chai.py
@@ -0,0 +1,8 @@
+# A chai shop makes tea in batches every 15 minutes.
+# You want to simulate 4 batches.
+# Task:
+    # Use range() to simulate batch numbers.
+    # Print: "Prepring chai for batch #[number]"
+
+for batch in range(1, 5):
+    print(f"Preparing chai for batch #{batch}")
---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,10 @@
+# You receive a list of names for chai orders.
+# The goal is to print out the order queue.
+# Task: 
+    # Use a list of names.
+    # Print: "Order ready for [name]"
+
+orders = ["sahil", "Aman", "Boon", "Charles"]
+
+for name in orders:
+    print(f"Order ready for {name}")

f0afcd1977e9fbb9f77a17a1b814fbf44402c962
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-21 13:05:41 +0530

    Added more practice questions.

--- Questions_Practice/Practice_questions_02.py
+++ Questions_Practice/Practice_questions_02.py
@@ -20,3 +20,33 @@ if len(password) >= minimum_length:
     print("Password is vaild!")
 elif len(password) < minimum_length:
     print(f"Password is Invalid. Must be at least {minimum_length} characters.")
+
+
+# Attendance Counter
+
+attendance = ["P", "A", "P"]
+present_count = 0
+
+for attend in attendance:
+    if attend == "P":
+        present_count += 1
+
+print(f"The total number of students presents are: {present_count}")
+
+
+# Online Shopping Discount
+
+total_amount = 6001
+
+if total_amount >= 5000:
+    discount = 20
+elif total_amount >= 2000:
+    discount = 10
+else:
+    discount = 0
+
+discount_amount = (total_amount % discount) / 100
+final_price = total_amount - discount_amount
+
+print(f"You got a discount of {discount}%")
+print(f"The final price after a {discount}% discount is: ${final_price}")

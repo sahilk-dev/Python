7e4815f290534aedf6c9b81190cd676dd0241667
Sahil Kamila <sahilkamila143@gmail.com>
2026-04-08 18:30:05 +0530

    About import, modules and init files

---
+++ 06_chai_business/main.py
@@ -0,0 +1,9 @@
+import recipes.flavours
+
+print(recipes.flavours.ginger_chai())
+
+
+from recipes.flavours import elachi_chai, ginger_chai
+
+print(ginger_chai())
+
---
+++ 06_chai_business/recipes/flavours.py
@@ -0,0 +1,5 @@
+def elachi_chai():
+    return "Elachi chai is ready"
+
+def ginger_chai():
+    return "Ginger chai is ready"

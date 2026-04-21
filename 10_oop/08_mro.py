46019b04dbbd9ff7e5416b2536dd5ac5a73d9d20
Sahil Kamila <sahilkamila143@gmail.com>
2026-04-21 18:26:00 +0530

    Learn about ways to access base class & mro

---
+++ 10_oop/08_mro.py
@@ -0,0 +1,17 @@
+# Method Resolution Order
+
+class A:
+    label = "A: Base class"
+
+class B(A):
+    label = "B: Masala blend"
+
+class C(A):
+    label = "C: Herbal blend"
+
+class D(B, C):
+    pass
+
+cup = D()
+print(cup.label)
+print(D.__mro__)
